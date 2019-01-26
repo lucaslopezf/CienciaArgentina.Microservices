f79b6543b09d9c416fc0f277707edb0e74cfe3be
Lucas Francisco López <lucaslopezf@gmail.com>
2019-01-26 20:04:30 -0300

    Add UserController

---
+++ CienciaArgentina.Microservices.Entities/Dtos/UserCreateDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CienciaArgentina.Microservices.Entities.Dtos
+{
+    public class UserCreateDto
+    {
+        [Required(ErrorMessage = "Por favor ingresa un nombre de usuario")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Por favor ingresa una contraseña")]
+        public string Password { get; set; }
+    }
+}
--- CienciaArgentina.Microservices.Entities/Models/UserModel.cs
+++ CienciaArgentina.Microservices.Entities/Models/UserModel.cs
@@ -7,7 +7,7 @@ namespace CienciaArgentina.Microservices.Entities.Models
         public Guid Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-        public bool Confirmeg { get; set; }
+        public bool Confirmed { get; set; }
         public DateTime DateFrom { get; set; }
         public DateTime DateTo { get; set; }
         public bool Blocked { get; set; }
