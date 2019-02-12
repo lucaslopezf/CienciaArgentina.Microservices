6d34ee9a2523d8bbfe1dc69f53e06ad0f4a91348
Lucas Francisco López <lucaslopezf@gmail.com>
2019-02-12 20:10:08 -0300

    LogRepository

---
+++ CienciaArgentina.Microservices.Data/Repositories/LogRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using CienciaArgentina.Microservices.Data.IRepositories;
+using CienciaArgentina.Microservices.Entities.Identity;
+using CienciaArgentina.Microservices.Entities.Models;
+using CienciaArgentina.Microservices.Entities.QueryParameters;
+using CienciaArgentina.Microservices.Storage.Azure.TableStorage.Queries;
+using CienciaArgentina.Microservices.Storage.Azure;
+using CienciaArgentina.Microservices.Storage.Azure.TableStorage;
+using Microsoft.AspNetCore.Identity;
+
+namespace CienciaArgentina.Microservices.Data.Repositories
+{
+    public class LogRepository : ILogRepository
+    {
+        private static readonly AppExceptionQuery azureStorage = new AppExceptionQuery(AzureStorageAccount.DefaultAccount);
+
+        public async Task<IQueryable<AppExceptionData>> Get(QueryParameters logQueryParameters)
+        {
+
+            IQueryable<AppExceptionData> listExceptions;
+
+            if (!logQueryParameters.HasQuery)
+                listExceptions = await azureStorage.GetExceptions();
+            else
+                listExceptions = await azureStorage.GetExceptions(logQueryParameters);
+
+            if(logQueryParameters.Descending)
+                return listExceptions.OrderByDescending(x => x.Date)
+                    .Skip(logQueryParameters.PageCount * (logQueryParameters.Page - 1))
+                    .Take(logQueryParameters.PageCount);
+
+            return listExceptions.OrderBy(x => x.Date)
+                .Skip(logQueryParameters.PageCount * (logQueryParameters.Page - 1))
+                .Take(logQueryParameters.PageCount);
+        }
+
+        public async Task<AppExceptionData> Get(string id)
+        {
+            var exception = await azureStorage.GetExceptions(id);
+            return exception;
+        }
+
+        //TODO: Count
+        public int Count()
+        {
+            return 1;
+        }
+    }
+}
