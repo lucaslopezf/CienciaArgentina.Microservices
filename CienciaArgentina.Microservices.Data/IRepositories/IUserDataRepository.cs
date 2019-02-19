6f670f08073435a7f45c281c396e96488ef6dfba
Nicolás <nicolasecamilo@gmail.com>
2019-02-18 23:17:11 -0300

    Moved DTOs

--- CienciaArgentina.Microservices.Data/IRepositories/IUserDataRepository.cs
+++ CienciaArgentina.Microservices.Data/IRepositories/IUserDataRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using CienciaArgentina.Microservices.Entities.Dtos;
+using CienciaArgentina.Microservices;
 using CienciaArgentina.Microservices.Entities.Models;
 using CienciaArgentina.Microservices.Entities.QueryParameters;
 
