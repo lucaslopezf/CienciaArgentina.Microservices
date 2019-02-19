ebf930c839e9227293a0f1baccf58c4cd50b1b17
Lucas Francisco López <lucaslopezf@gmail.com>
2019-02-19 00:43:49 -0300

    Auto stash before merge of "master" and "CAMicroservices/master"

---
+++ CienciaArgentina.Microservices.Entities/Models/JobOffer/JobOfferUserLanguajeKnoweldge .cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using CienciaArgentina.Microservices.Entities.Identity;
+using CienciaArgentina.Microservices.Entities.Models.User;
+
+namespace CienciaArgentina.Microservices.Entities.Models.JobOffer
+{
+    public class JobOfferUserLanguajeKnoweldge
+    {
+        [Key]
+        public int Id { get; set; }
+        public JobOffer JobOffer { get; set; }
+        public string Condition { get; set; }
+    }
+}
