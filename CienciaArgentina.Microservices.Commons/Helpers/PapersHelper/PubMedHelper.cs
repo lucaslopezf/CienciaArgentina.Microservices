586382e27bd623f0449f61eeff6b8d4d28a072be
Nicolás <nicolasecamilo@gmail.com>
2019-03-06 20:43:11 -0300

    get pubmed articles by alias

--- CienciaArgentina.Microservices.Commons/Helpers/PapersHelper/PubMedHelper.cs
+++ CienciaArgentina.Microservices.Commons/Helpers/PapersHelper/PubMedHelper.cs
@@ -16,7 +16,8 @@ namespace CienciaArgentina.Microservices.Commons.Helpers.PapersHelper
     {
         // First: https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=29519839&usehistory=y
         // Then: https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&query_key=1&WebEnv=NCID_1_23914615_130.14.18.97_9001_1551671826_1134467423_0MetA0_S_MegaStore
-        public static async Task<Tuple<string, string>> SearchPMID(int? pmid)
+        // https://www.ncbi.nlm.nih.gov/books/NBK25497/
+        public static async Task<Tuple<string, string>> SearchByPMIDAsync(int? pmid)
         {
             // Query string for PubMed search
             var url = $"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term={pmid}&usehistory=n";
@@ -24,6 +25,8 @@ namespace CienciaArgentina.Microservices.Commons.Helpers.PapersHelper
             var result = await httpClient.GetAsync(url);
             var stringResponse = await result.Content.ReadAsStringAsync();
             var xml = XDocument.Parse(stringResponse).LastNode;
+            if(xml == null)
+                throw new NullReferenceException("No se encontró nada para la búsqueda asignada");
             var count = xml.XPathSelectElement("//Count");
             if (count == null || Convert.ToInt32(count.Value) == 0)
                 throw new NullReferenceException("No se encontró nada para la búsqueda asignada");
@@ -32,7 +35,7 @@ namespace CienciaArgentina.Microservices.Commons.Helpers.PapersHelper
 
             return elements;
         }
-        public static async Task<PaperDto> SearchPMIDResults(Tuple<string, string> search)
+        public static async Task<PaperDto> SearchPMIDResultsAsync(Tuple<string, string> search)
         {
             var url =
                 $"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&query_key={search.Item1}&WebEnv={search.Item2}";
@@ -60,6 +63,33 @@ namespace CienciaArgentina.Microservices.Commons.Helpers.PapersHelper
             paper.LastAuthor = xml.Descendants("Item").FirstOrDefault(x => x.Attribute("Name")?.Value == "LastAuthor").Value;
 
             return paper;
+        }
+
+        public static async Task<List<string>> SearchByAliasAsync(string alias)
+        {
+            var pmidList = new List<string>();
+            var encodedAlias = EncodeAlias(alias);
+            var url = $"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term={encodedAlias}%5BAuthor%5D&usehistory=n";
+            var httpClient = new HttpClient();
+            var result = await httpClient.GetAsync(url);
+            var stringResponse = await result.Content.ReadAsStringAsync();
+            var xml = XDocument.Parse(stringResponse).LastNode;
+            if (xml == null)
+                throw new NullReferenceException("No se encontraron papers para este alias");
+            var count = xml.XPathSelectElement("//Count");
+            if (count == null || Convert.ToInt32(count.Value) == 0)
+                throw new NullReferenceException("No se encontraron papers para este alias");
+            var idList = xml.XPathSelectElement("//IdList");
+            foreach (var id in idList?.Elements("Id"))
+            {
+                pmidList.Add(id.Value);
+            }
+            return pmidList;
+        }
+
+        private static string EncodeAlias(string alias)
+        {
+            return alias.Replace(" ", "%20");
         }
     }
 }
