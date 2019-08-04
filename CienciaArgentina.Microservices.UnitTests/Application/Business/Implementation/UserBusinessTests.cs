8825572443dbf8c0e363f7e47041a6921a94e505
Nico <nicolasecamilo@gmail.com>
2019-08-04 15:22:41 -0300

    test folder structure and UserBusinessTests

---
+++ CienciaArgentina.Microservices.UnitTests/Application/Business/Implementation/UserBusinessTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CienciaArgentina.Microservices.Tests.Application.Business.Implementation
+{
+    public class UserBusinessTests
+    {        
+        public class AddMethod
+        {
+            // Test 1, Story 6
+            [Fact]
+            public void SuccessfulRegister()
+            {
+                
+            }
+        }
+            
+    }
+}
