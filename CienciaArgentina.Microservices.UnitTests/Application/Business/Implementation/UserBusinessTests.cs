97e47d6ef0068e6e9af8e57c9efd4e03403e06f4
Nico <nicolasecamilo@gmail.com>
2019-08-04 16:38:00 -0300

    minor changes

--- CienciaArgentina.Microservices.UnitTests/Application/Business/Implementation/UserBusinessTests.cs
+++ CienciaArgentina.Microservices.UnitTests/Application/Business/Implementation/UserBusinessTests.cs
@@ -1,19 +1,25 @@
-﻿using System;
+﻿using CienciaArgentina.Microservices.Entities.Identity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
+using Moq;
+using CienciaArgentina.Microservices.Business.Interfaces;
+using CienciaArgentina.Microservices.Business;
+using CienciaArgentina.Microservices.Entities.BusinessModel;
+using CienciaArgentina.Microservices.Repositories.IUoW;
 
 namespace CienciaArgentina.Microservices.Tests.Application.Business.Implementation
 {
     public class UserBusinessTests
-    {        
+    {
         public class AddMethod
         {
             // Test 1, Story 6
-            [Fact]
-            public void SuccessfulRegister()
+            [Fact(Skip = "Not implemented yet")]
+            public void Add_CorrectParams_SuccessfulRegister()
             {
-                
+
             }
         }
             
